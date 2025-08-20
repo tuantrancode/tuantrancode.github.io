e05cb906c2c6cd8db6560297462cd98e1474b5c1
tuantrancode <tuantran.inbox@gmail.com>
2025-08-20 13:26:34 -0500

    Starting repo

---
+++ react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '40px'}}>
+      <h1>Hello from React!</h1>
+      <p>This is the React version of the site.</p>
+    </div>
+  );
+}
+export default App;
