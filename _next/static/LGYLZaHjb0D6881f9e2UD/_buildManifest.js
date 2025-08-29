d3e59034cd2cb36ee4690db10816b5fcc3eddc02
tuantrancode <tuantran.inbox@gmail.com>
2025-08-29 18:38:48 +0000

    Deploying to react-live from @ tuantrancode/tuantrancode.github.io@ae5b913656ca2740e21d5e16a5c4a3b1056b364a 🚀

--- _next/static/LGYLZaHjb0D6881f9e2UD/_buildManifest.js
+++ _next/static/LGYLZaHjb0D6881f9e2UD/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/fa78830d85084b1e.js"
+    "./static/chunks/71e397e2a7b231ad.js"
   ],
   "__rewrites": {
     "afterFiles": [],
---
+++ _next/static/chunks/f9ac690b782fd3bd.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/29fcbd23695f5ab4.js",
+  "static/chunks/turbopack-32e9528c36726697.js"
+])
